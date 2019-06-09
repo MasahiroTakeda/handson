65b436f3e92ec270fcbfecabc2a5a72f0cccc5d1
MasahiroTakeda <taketake.12326@gmail.com>
2019-06-09 13:01:10 +0900

    よろしく

---
+++ tutorial/flaskr/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('flaskr.config')
+
+db = SQLAlchemy(app)
+
+import flaskr.views
+
+##from flask.ext.sqlalchemy import SQLAlchemy
